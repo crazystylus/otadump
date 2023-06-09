891d55357b7a1422c11f1cea8b007bdbfa64ab2d
Ajeet D'Souza <98ajeet@gmail.com>
2023-06-10 02:28:55 +0530

    Remove unneeded proto fields (#18)
    Co-authored-by: Kartik Sharma <kartik.sharma522@gmail.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     prost_build::compile_protos(
         &["src/protos/chromeos_update_engine/update_metadata.proto"],
-        &["src/protos/"],
+        &[] as &[&str],
     )
     .expect("error compiling protobuf files");
 }
